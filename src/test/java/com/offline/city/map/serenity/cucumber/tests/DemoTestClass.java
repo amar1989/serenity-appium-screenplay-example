e14156d74069e9369d81aad129b4f0df99589147
amar singh <amarsingh@amars-MacBook-Pro.local>
2023-03-04 04:28:21 -0800

    Ading test cases

---
+++ src/test/java/com/offline/city/map/serenity/cucumber/tests/DemoTestClass.java
@@ -0,0 +1,12 @@
+package com.offline.city.map.serenity.cucumber.tests;
+
+import org.junit.Test;
+
+public class DemoTestClass {
+
+    @Test
+    public void demoTest()
+    {
+        System.out.println("Hello World");
+    }
+}
